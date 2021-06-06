a5b13d1560c554a1962622e425acb9172326165b
Chris Goslin <cgoslin7@gmail.com>
2021-06-06 03:47:31 -0500

    ad Procfile

--- public/db.js
+++ public/db.js
@@ -15,7 +15,7 @@ request.onsuccess = function(event) {
 };
 
 request.onerror = function(event) {
-  console.log("Whoops! " + event.target.errorCode);
+  console.log("error" + event.target.errorCode);
 };
 
 function saveRecord(record) {
